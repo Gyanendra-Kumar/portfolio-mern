7cce5d6af49a90a1b772fb5ef3a27edf1a9241fb
Huski-commando <gyan.mu@gmail.com>
2024-01-27 02:40:45 +0530

    created header section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,61 @@
 import React from "react";
-import { Navbar } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Button, Navbar, NavbarToggle, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
+  const path = useLocation().pathname;
+  //   const pathname = path.split("/").filter((x) => x);
+  //   console.log(pathname);
+
   return (
     <Navbar className="border-b-2">
       <Link
         to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white tracking-wide"
       >
-        <span>Port</span>
-        Folio
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md text-white ">
+          Gyanendra's
+        </span>
+        PortFolio
       </Link>
+
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+
+      <Button className="w-12 h-10 lg:hidden" color="gray">
+        <AiOutlineSearch />
+      </Button>
+
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:flex" color="gray">
+          <FaMoon />
+        </Button>
+
+        <Link to="/sign-in">
+          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+        </Link>
+        <NavbarToggle />
+      </div>
+
+      <Navbar.Collapse>
+        <Navbar.Link as={"div"} active={path === "/"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link as={"div"} active={path === "/about"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link as={"div"} active={path === "/projects"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
