df5ffcdf28939bfe4012caf75d8c4b0e4ca1406f
Huski-commando <gyan.mu@gmail.com>
2024-01-27 12:12:03 +0530

    created server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -40,7 +40,9 @@ const Header = () => {
         </Button>
 
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>
+            Sign In
+          </Button>
         </Link>
         <NavbarToggle />
       </div>
