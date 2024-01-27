4007ef6d6c79ce2a78dcd15ccf532ca6aad073d6
Huski-commando <gyan.mu@gmail.com>
2024-01-27 13:52:24 +0530

    created test routes- /localhost:3000/api/user/test

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
 
 // connect to DB
 connectDB();
@@ -12,6 +13,9 @@ const app = express();
 
 // Enable cors for all routes
 app.use(cors());
+
+// routes for api
+app.use("/api/user", userRoutes);
 
 app.listen(3000, () => {
   console.log(`Server is listening on port 3000!`);
