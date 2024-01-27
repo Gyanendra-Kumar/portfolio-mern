dfd8c239203adc5f62913f8e2e9a4a59b5c04e3d
Huski-commando <gyan.mu@gmail.com>
2024-01-28 02:06:21 +0530

    created sign up functionality and added to backend

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,61 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Label, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [id]: value.trim(),
+    }));
+  };
+
+  // console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(formData);
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+
+    try {
+      setIsLoading(true);
+      setErrorMessage(null);
+
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      // console.log(data);
+      if (data.success === false) {
+        setIsLoading(false);
+        return setErrorMessage(data.message);
+      }
+
+      setIsLoading(false);
+      navigate("/sign-in");
+    } catch (error) {
+      setErrorMessage(error.message);
+      setIsLoading(false);
+      return;
+    }
+  };
+
   return (
-    <div className="min-h-screen mt-20">
+    <div className="max-h-screen mt-20">
       <div className="flex gap-4 md:gap-10 p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
         {/* left */}
         <div className="text-center flex-1">
@@ -25,7 +76,7 @@ const SignUp = () => {
 
         {/* right */}
         <div className="flex-1 flex flex-col gap-4">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Username" />
               <TextInput
@@ -33,6 +84,8 @@ const SignUp = () => {
                 placeholder="Enter Username"
                 id="username"
                 required
+                value={formData.username}
+                onChange={handleInputChange}
               />
             </div>
 
@@ -43,21 +96,35 @@ const SignUp = () => {
                 placeholder="Enter Email"
                 id="email"
                 required
+                value={formData.email}
+                onChange={handleInputChange}
               />
             </div>
 
             <div>
               <Label value="Password" />
               <TextInput
-                type="text"
+                type="password"
                 placeholder="Enter Password"
                 id="password"
                 required
+                value={formData.password}
+                onChange={handleInputChange}
               />
             </div>
 
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign Up
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <>
+                  <Spinner size="sm" /> <span className="pl-3">Loading</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
 
@@ -67,6 +134,12 @@ const SignUp = () => {
               Sign In
             </Link>
           </div>
+
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
