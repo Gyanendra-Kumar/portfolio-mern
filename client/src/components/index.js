b83290beafc3c1aa5a990b2bc134cff37abd28d1
Huski-commando <gyan.mu@gmail.com>
2024-01-29 14:19:34 +0530

    started working on dashboard

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Header } from "./Header";
 export { default as OAuth } from "./OAuth";
+export { default as DashboardSidebar } from "./DashboardSidebar";
+export { default as DashboardProfile } from "./DashboardProfile";
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { DashboardProfile, DashboardSidebar } from "../components";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    // console.log(tabFromUrl);
+
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <div>
+      <div className="">
+        {/* sidebar */}
+        <DashboardSidebar />
+      </div>
+
+      {/* profile */}
+      {tab === "profile" && <DashboardProfile />}
+    </div>
+  );
 };
 
 export default Dashboard;
