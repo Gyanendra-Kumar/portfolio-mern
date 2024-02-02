15705563de6ba109565c133e076d956e49751fc0
Huski-commando <gyan.mu@gmail.com>
2024-02-02 15:02:33 +0530

    fetching all posts from endpoint

--- client/src/components/DashboardPosts.jsx
+++ client/src/components/DashboardPosts.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const DashboardPosts = () => {
+  const [userPosts, setUserPosts] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getPosts?userId=${currentUser._id}`);
+        const data = await res.json();
+
+        if (res.ok) {
+          setUserPosts(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (currentUser.isAdmin || currentUser.isEditor) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+
+  console.log(userPosts);
   return <div>DashboardPosts</div>;
 };
 
