e38c5cc110f82ac83cf4f2e7e93cb449905b6309
Huski-commando <gyan.mu@gmail.com>
2024-02-04 22:20:30 +0530

    created getAllUsers api endpoints

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -77,3 +77,47 @@ export const signOut = (req, res, next) => {
     next(error);
   }
 };
+
+// get all users from DB
+export const getAllUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(
+      errorHandler(403, "You are not authorized to access the page.")
+    );
+  }
+
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 10;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthUser = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
