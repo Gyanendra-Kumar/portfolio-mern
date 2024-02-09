17be29db24caf2966081662c3272792fd46dfc24
Huski-commando <gyan.mu@gmail.com>
2024-02-09 11:50:19 +0530

    able to recieve email from user

--- client/src/pages/ContactMe.jsx
+++ client/src/pages/ContactMe.jsx
@@ -1,7 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactMe = () => {
-  return <div className="max-w-5xl mx-auto">ContactMe</div>;
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [stateMessage, setStateMessage] = useState(null);
+  const [formData, setFormData] = useState({
+    user_name: "",
+    user_email: "",
+    message: "",
+    user_mobile: 0,
+  });
+
+  const sendEmail = (e) => {
+    e.persist();
+    e.preventDefault();
+    setIsSubmitting(true);
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAIL_JS_SERVICE_ID,
+        import.meta.env.VITE_EMAIL_JS_TEMPLATE_ID,
+        e.target,
+        import.meta.env.VITE_EMAIL_JS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          setStateMessage("Message sent!");
+          setIsSubmitting(false);
+          setTimeout(() => {
+            setStateMessage(null);
+          }, 5000); // hide message after 5 seconds
+        },
+        (error) => {
+          setStateMessage("Something went wrong, please try again later");
+          setIsSubmitting(false);
+          setTimeout(() => {
+            setStateMessage(null);
+          }, 5000); // hide message after 5 seconds
+        }
+      );
+
+    // Clears the form after sending the email
+    e.target.reset();
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="max-w-5xl mx-auto min-h-screen">
+      <h1>Contact Me</h1>
+      <form onSubmit={sendEmail} className="flex flex-col mt-6 w-[50%]">
+        <label>Name</label>
+        <input
+          type="text"
+          name="user_name"
+          value={formData.user_name}
+          onChange={handleChange}
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          name="user_email"
+          value={formData.user_email}
+          onChange={handleChange}
+        />
+        <label>Mobile Number:</label>
+        <input
+          type="number"
+          name="user_mobile"
+          value={formData.user_mobile}
+          onChange={handleChange}
+        />
+        <label>Message</label>
+        <textarea
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+        />
+        <button type="submit" disabled={isSubmitting}>
+          Submit
+        </button>
+        {stateMessage && <p>{stateMessage}</p>}
+      </form>
+    </div>
+  );
 };
 
 export default ContactMe;
