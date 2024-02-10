5bf54805120a6fdf8d774b2dc2d996e801878fa4
Huski-commando <gyan.mu@gmail.com>
2024-02-10 13:41:58 +0530

    working on contact section

--- client/src/pages/ContactMe.jsx
+++ client/src/pages/ContactMe.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
 import emailjs from "@emailjs/browser";
+import {
+  Button,
+  Card,
+  Checkbox,
+  Label,
+  TextInput,
+  Textarea,
+} from "flowbite-react";
+import contact from "../assets/contact1.png";
 
 const ContactMe = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -49,40 +58,76 @@ const ContactMe = () => {
 
   return (
     <div className="max-w-5xl mx-auto min-h-screen">
-      <h1>Contact Me</h1>
-      <form onSubmit={sendEmail} className="flex flex-col mt-6 w-[50%]">
-        <label>Name</label>
-        <input
-          type="text"
-          name="user_name"
-          value={formData.user_name}
-          onChange={handleChange}
-        />
-        <label>Email</label>
-        <input
-          type="email"
-          name="user_email"
-          value={formData.user_email}
-          onChange={handleChange}
-        />
-        <label>Mobile Number:</label>
-        <input
-          type="number"
-          name="user_mobile"
-          value={formData.user_mobile}
-          onChange={handleChange}
-        />
-        <label>Message</label>
-        <textarea
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-        />
-        <button type="submit" disabled={isSubmitting}>
-          Submit
-        </button>
-        {stateMessage && <p>{stateMessage}</p>}
-      </form>
+      <h1 className="text-center text-3xl md:text-6xl mt-20 font-serif text-gray-600 dark:text-gray-200">
+        Contact Me
+      </h1>
+
+      <div className="flex flex-col lg:flex-row gap-6 mt-10">
+        {/* contact details */}
+        <Card className="flex-1 py-10 bg-gradient-to-br hover:bg-gradient-to-tr from-gray-200 via-gray-50 to-white dark:from-[#1f2937] dark:via-[#2c3a4d] dark:shadow-2xl shadow-xl">
+          <img />
+        </Card>
+
+        {/* contact form */}
+        <Card className="flex-1 py-10 bg-gradient-to-br hover:bg-gradient-to-tr from-gray-200 via-gray-50 to-white dark:from-[#1f2937] dark:via-[#2c3a4d] dark:shadow-2xl shadow-xl">
+          <form onSubmit={sendEmail} className="flex flex-col  gap-2">
+            <div className="flex flex-col gap-3">
+              <Label>
+                <span className="text-red-400">*</span>Name:
+              </Label>
+              <TextInput
+                type="text"
+                name="user_name"
+                value={formData.user_name}
+                onChange={handleChange}
+                placeholder="Enter your Name."
+                required
+              />
+            </div>
+            <Label>
+              <span className="text-red-400">*</span>Email
+            </Label>
+            <TextInput
+              type="email"
+              name="user_email"
+              value={formData.user_email}
+              onChange={handleChange}
+              required
+              placeholder="Enter your email"
+            />
+            <Label>
+              <span className="text-red-400">*</span>Mobile Number:
+            </Label>
+            <TextInput
+              type="number"
+              name="user_mobile"
+              value={formData.user_mobile}
+              onChange={handleChange}
+              required
+            />
+            <Label>
+              <span className="text-red-400">*</span>Message
+            </Label>
+            <Textarea
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              rows={4}
+              placeholder="Leave a message"
+              required
+            />
+            <Button
+              type="submit"
+              disabled={isSubmitting}
+              className="mt-2"
+              gradientDuoTone="purpleToPink"
+            >
+              Submit
+            </Button>
+            {stateMessage && <p>{stateMessage}</p>}
+          </form>
+        </Card>
+      </div>
     </div>
   );
 };
